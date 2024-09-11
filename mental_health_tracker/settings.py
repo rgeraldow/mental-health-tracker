4160303788af42d2f2050f8100ba6ad0fdbd41b9
rgeraldow <rgeraldow@gmail.com>
2024-09-11 14:11:17 +0700

    edit link url pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://rogerio.geraldo/mentalhealthtracker", "https://rogerio.geraldo/mentalhealthtracker"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/rogerio.geraldo/mentalhealthtracker", "https://pbp.cs.ui.ac.id/rogerio.geraldo/mentalhealthtracker"]
